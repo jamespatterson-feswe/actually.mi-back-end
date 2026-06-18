48a21a6b7aabe9a6f3f1b9928d5bc5870b81a381
James Patterson <jamespatterson.swe@gmail.com>
2026-06-18 00:40:23 -0400

    build: prisma models, add routes auth, specifically add register

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+/** imported routes */
+import authRoutes from './routes/auth.routes';
+
 dotenv.config();
 
 const server = express();
 const PORT = process.env.PORT || 8080;
 
 server.use(cors());
-server.use(express());
+server.use(express.json());
+
+server.use('/auth', authRoutes);
 
 server.get('/health', (req, res) => {
   res.json({ status: 200, statusDesc: 'Server is healthy and running correctly.' });
@@ -17,3 +22,4 @@ server.get('/health', (req, res) => {
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 })
+
