627e3ab31b5f1b91cfbf09874eb8a386287bcf23
James Patterson <jamespatterson.swe@gmail.com>
2026-06-23 21:07:59 -0400

    fix: no need to reinitialize the app to server in index ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import app from './app';
 
-const server = app;
-
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 })
 
